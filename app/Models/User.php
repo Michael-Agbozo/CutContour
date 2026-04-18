f02dfb26138e1fbce8d0ce1ed611aa82ea740b00
Michael Agbozo <agbozomykell8@gmail.com>
2026-04-18 18:45:36 +0000

    feat: add profile avatar upload and display

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,10 +10,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 
-#[Fillable(['name', 'email', 'password', 'is_admin', 'usage_reset_at'])]
+#[Fillable(['name', 'email', 'avatar_path', 'password', 'is_admin', 'usage_reset_at'])]
 #[Hidden(['password', 'two_factor_secret', 'two_factor_recovery_codes', 'remember_token'])]
 class User extends Authenticatable implements MustVerifyEmailContract
 {
@@ -45,6 +46,11 @@ class User extends Authenticatable implements MustVerifyEmailContract
             ->take(2)
             ->map(fn ($word) => Str::substr($word, 0, 1))
             ->implode('');
+    }
+
+    public function avatarUrl(): ?string
+    {
+        return $this->avatar_path ? Storage::url($this->avatar_path) : null;
     }
 
     /** All cut jobs owned by this user. */
